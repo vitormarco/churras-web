a98229d7170498a39b21d136dd884762cf865672
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-06 17:14:38 -0300

    fix some problems

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  singleQuote: true,        // Para utilizar aspas simples
+  trailingComma: 'all',     // para adicionar virgula sempre ao final de um objeto que tenha sido quebrado
+  arrowParens: 'avoid',     // Para que não seja adicionado Arrow Function se tiver apenas um parametro
 }
