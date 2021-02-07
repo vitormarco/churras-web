3ad0f51ca349cf2a030969a3f3c789664b73b4e3
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-07 16:24:36 -0300

    Iniciando estrutura para detalhe do appointment

---
+++ src/pages/AppointmentDetail/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppointmentDetail: React.FC = () => (
+  <h1>Detail</h1>
+);
+
+export default AppointmentDetail;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,13 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
 import Appointments from '../pages/Appointments';
+import AppointmentDetail from '../pages/AppointmentDetail';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/appointments" component={Appointments} isPrivate />
+    <Route path="/appointment/:id_appointment/show" component={AppointmentDetail} isPrivate />
   </Switch>
 );
 
